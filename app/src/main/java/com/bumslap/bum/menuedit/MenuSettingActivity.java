25986059a51756ab62b9d13a77a676c6804f0039
KwonOyoun <koy9303@gmail.com>
2017-12-06 15:54:26 +0900

    please git hub.. add intent menu update image, button

--- app/src/main/java/com/bumslap/bum/menuedit/MenuSettingActivity.java
+++ app/src/main/java/com/bumslap/bum/menuedit/MenuSettingActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumslap.bum.POSproject.R;
@@ -17,12 +18,24 @@ public class MenuSettingActivity extends AppCompatActivity {
         ImageView BTNMenuUpdate = (ImageView)findViewById(R.id.imageView);
         BTNMenuUpdate.setOnClickListener(updatemenu);
 
+        Button BTNMenuUpdate2 = (Button)findViewById(R.id.button);
+        BTNMenuUpdate2.setOnClickListener(updatemenu2);
+
     }
+
+    Button.OnClickListener updatemenu2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), MenuUpdateActivity.class);
+            startActivity(intent);
+        }
+    };
 
     ImageView.OnClickListener updatemenu = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(getApplicationContext(), MenuUpdateActivity.class);
+            startActivity(intent);
         }
     };
 }
