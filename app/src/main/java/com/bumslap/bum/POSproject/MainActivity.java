48f21b2b683d3130107d8213a509f0edbd0b8982
KwonOyoun <koy9303@gmail.com>
2017-12-06 13:29:28 +0900

    Intent as possible as ...

--- app/src/main/java/com/bumslap/bum/POSproject/MainActivity.java
+++ app/src/main/java/com/bumslap/bum/POSproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bumslap.bum.POSproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button BtnStart = (Button)findViewById(R.id.button_Start);
+        BtnStart.setOnClickListener(BtnClick);
+
+        Button BtnPreparet = (Button)findViewById(R.id.button_Prepare);
+        BtnPreparet.setOnClickListener(BtnClick);
+
+        Button BtnAnalysis = (Button)findViewById(R.id.button_Analysis);
+        BtnAnalysis.setOnClickListener(BtnClick);
+
+        Button BtnSetting = (Button)findViewById(R.id.button_Setting);
+        BtnSetting.setOnClickListener(BtnClick);
     }
+
+    Button.OnClickListener BtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.button_Start:
+                    Intent intent = new Intent(getApplicationContext(), OrderActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.button_Prepare:
+                    intent = new Intent(getApplicationContext(), MenuSettingActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.button_Analysis:
+                    intent = new Intent(getApplicationContext(), PieChartActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.button_Setting:
+                    intent = new Intent(getApplicationContext(), UserSettingActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
 }
--- app/src/main/java/com/bumslap/bum/POSproject/MenuSettingActivity.java
+++ app/src/main/java/com/bumslap/bum/POSproject/MenuSettingActivity.java
@@ -1,7 +1,12 @@
 package com.bumslap.bum.POSproject;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MenuSettingActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MenuSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_setting);
+        ImageView BTNMenuUpdate = (ImageView)findViewById(R.id.imageView);
+        BTNMenuUpdate.setOnClickListener(updatemenu);
+
     }
+
+    ImageView.OnClickListener updatemenu = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), MenuUpdateActivity.class);
+        }
+    };
 }
