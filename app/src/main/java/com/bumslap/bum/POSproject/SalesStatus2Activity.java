881614b666317da44b845870a7dfa7f391961038
venturermin <ito76@naver.com>
2017-12-06 10:28:55 +0900

    change again

---
+++ app/src/main/java/com/bumslap/bum/POSproject/SalesStatus2Activity.java
@@ -0,0 +1,13 @@
+package com.bumslap.bum.POSproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalesStatus2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_status2);
+    }
+}
